43554a92c1be7f060609c7397bb191f725052d42
Your Name <your@email.com>
2026-05-07 15:00:11 +0100

    fix cors

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from threading import Thread
 from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 bot_state = {
     "running": True,
